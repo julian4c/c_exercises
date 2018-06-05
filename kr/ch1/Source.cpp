c126a95229b2a66e23be78b499575c8dc06ee396
Julian <jruiz@dextratech.com>
2018-06-05 13:50:09 -0500

    Added EXIT_SUCCESS from stdlib.h

--- kr/ch1/Source.cpp
+++ kr/ch1/Source.cpp
@@ -1,7 +1,36 @@
-#include <stdio.h>
+/*
+============================================================================
+Name        : Source.cpp
+Author      : Julian
+Version     :
+Copyright   : Your copyright notice
+Description : Hello World in C, Ansi-style
+============================================================================
+*/
 
-int main()
-{
-	printf("Hello World");
-	return 0;
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+
+	int i = 0;
+	int *p;
+	//
+	//	Tip: Let the compiler check errors:
+	//
+	static union
+	{
+		char error_int8[sizeof(signed char) == 1];
+		char error_uint8[sizeof(unsigned char) == 1];
+	};
+
+	puts("!!!Hello World!!!\n");
+	p = &i;
+	printf("Initial Value *p = %d\n", *p);
+	printf("test *p +1: %d\n", *p + 1);
+	printf("test *p +=1: %d\n", *p += 1);
+	printf("Final Value i = %d\n", i);
+
+
+	return EXIT_SUCCESS;
 }
